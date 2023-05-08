de4fcc443c2ec78bf9c5141b2cc105831f03575a
Ovais <ovais.akhtar@livebazaar.tv>
2023-05-08 20:05:24 +0500

    Initial Commit

---
+++ Instagram/features/home/controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by apple on 08/05/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
